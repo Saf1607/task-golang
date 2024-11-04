019e57763433e2d48b6931fc3f17968179e5357e
safira <safiraaisha1607@gmail.com>
2024-11-04 15:09:24 +0700

    new edit

--- main.go
+++ main.go
@@ -2,18 +2,40 @@ package main
 
 import (
 	"fmt"
-	"task-golang/utils"
+	"os"
+	"strconv"
+	"task-golang/utils" // Import the utils package
 )
 
 func main() {
+	// Read n from command line arguments
+	n := 1 // default value
+	if len(os.Args) > 1 {
+		var err error
+		n, err = strconv.Atoi(os.Args[1]) // convert string to int
+		if err != nil {
+			fmt.Println("Invalid number. Using default n=1")
+			n = 1
+		}
+	}
+
 	fmt.Println(utils.MagicSum(16))
 	fmt.Println(utils.MagicPow(3))
 	fmt.Println(utils.MagicOdd(4))
 	fmt.Println(utils.MagicGrade(4))
 	fmt.Println(utils.MagicName(2))
-	fmt.Println(utils.MagicTria(3))
 
-	n := 16
+	// MagicChange
+	fmt.Println("Before MagicChange:", n)
 	utils.MagicChange(&n)
-	fmt.Println("Changed value:", n)
+	fmt.Println("After MagicChange:", n)
+
+	// MagicTria
+	fmt.Println("MagicTria:", utils.MagicTria(n))
+
+	// Initialize MagicNumber struct and use Multiply method
+	magicNumber := utils.MagicNumber{Number: n}
+	fmt.Println("Before Multiply:", magicNumber.Number)
+	magicNumber.Multiply(n)
+	fmt.Println("After Multiply:", magicNumber.Number)
 }
--- utils/magic.go
+++ utils/magic.go
@@ -1,10 +1,11 @@
 package utils
 
+// MagicSum returns the sum of n + n
 func MagicSum(n int) int {
 	return n + n
 }
 
-// MagicPow takes an integer and returns n raised to the power of n
+// MagicPow returns n raised to the power of n
 func MagicPow(n int) int {
 	result := 1
 	for i := 0; i < n; i++ {
@@ -13,12 +14,12 @@ func MagicPow(n int) int {
 	return result
 }
 
-// MagicOdd takes an integer and returns true if it is an odd number
+// MagicOdd returns true if n is odd, otherwise false
 func MagicOdd(n int) bool {
 	return n%2 != 0
 }
 
-// MagicGrade returns a string representing the grade based on the input number
+// MagicGrade returns a string based on the grade
 func MagicGrade(n int) string {
 	switch n {
 	case 0:
@@ -38,11 +39,11 @@ func MagicGrade(n int) string {
 	}
 }
 
-// MagicName returns a slice of strings containing the name repeated n times
+// MagicName returns a slice filled with the name repeated n times
 func MagicName(n int) []string {
 	names := []string{}
 	for i := 0; i < n; i++ {
-		names = append(names, "safira") // Replace "radit" with your name
+		names = append(names, "safira")
 	}
 	return names
 }
@@ -56,7 +57,17 @@ func MagicTria(n int) int {
 	return sum
 }
 
-// MagicChange modifies the input integer by doubling its value
+// MagicChange changes the value of n to n * 2
 func MagicChange(n *int) {
-	*n = *n * 2
+	*n *= 2
+}
+
+// MagicNumber struct with a method to multiply its Number
+type MagicNumber struct {
+	Number int
+}
+
+// Multiply multiplies Number with n
+func (m *MagicNumber) Multiply(n int) {
+	m.Number *= n
 }
